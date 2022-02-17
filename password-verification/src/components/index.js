9c1934e5744f81b4cb6b7220bcd4aa9057083609
Derek Beckford <82908627+derekBeckford@users.noreply.github.com>
2022-02-16 18:04:24 -0600

    finished functioning and inputs for showing and hiding the password

--- password-verification/src/components/index.js
+++ password-verification/src/components/index.js
@@ -1,19 +1,30 @@
 import React, { useState } from "react";
 
 function PasswordVerify() {
-    //initiating a false boolean state with useState
-    const [shownPassword, setShownPassword] = useState(false);
-
-    //function to toggle shown or hiddent password
-    const togglePassword = () => {
-        setShownPassword(!shownPassword)
-    }
+  //initiating a false boolean state with useState
+  const [shownPassword, setShownPassword] = useState(false);
+  //initiating false state so that when a user clicks outside the checkbox but within the button they are able check the box.
+  var [checkBox, setCheckBox] = useState(false);
+  //function to toggle shown or hiddent password
+  const togglePassword = () => {
+    setShownPassword(!shownPassword);
+    setCheckBox(!checkBox);
+  };
 
   return (
     <section>
-        {/* if else for show password text or hide password text */}
-      <input type={shownPassword ? "text" : "password" }/>
-      <button onClick={togglePassword}> Show Password </button>
+      {/* if else for show password text or hide password text */}
+      <h3>Password</h3>
+      <input
+        type={shownPassword ? "text" : "password"}
+        className="passwordInput"
+      />
+      <button onClick={togglePassword}>
+        <span>
+          <input type="checkbox" checked={checkBox} onChange={togglePassword}/>
+          <label>Show</label>
+        </span>
+      </button>
     </section>
   );
 }
